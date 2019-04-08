ce4c1ae8757ceff3a21ac9375932e902fb670ecb
Pooja Ahuja <pcomputo@Poojas-MacBook-Pro.local>
2019-04-07 17:00:21 -0700

    classifier architecture

--- slide_classification.py
+++ slide_classification.py
@@ -9,14 +9,15 @@ from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 
 
-
 DATASET_DIR = 'image_classifier'
-IMAGE_HEIGHT = 64
-IMAGE_WIDTH = 64
+IMAGE_HEIGHT = 128
+IMAGE_WIDTH = 128
 BATCH_SIZE = 32
 VALIDATION_SPLIT = 0.2
+EPOCHS = 5
 
 
+#Labelled dataset creation
 train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, 
 	horizontal_flip = True, validation_split=VALIDATION_SPLIT)
 test_datagen = ImageDataGenerator(rescale = 1./255)
@@ -26,3 +27,17 @@ training_set = train_datagen.flow_from_directory(DATASET_DIR,
 validation_generator = train_datagen.flow_from_directory(DATASET_DIR, 
 	target_size=(IMAGE_HEIGHT, IMAGE_WIDTH), batch_size=BATCH_SIZE,
 	class_mode='binary', subset='validation')
+
+#classifier architecture
+classifier = Sequential()
+classifier.add(Conv2D(32, (3, 3), input_shape = (IMAGE_HEIGHT, IMAGE_WIDTH, 3), 
+	activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Flatten())
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+
+classifier.fit_generator(training_set, steps_per_epoch = 300, 
+	validation_data = validation_generator, validation_steps = 32, 
+	epochs = EPOCHS)
