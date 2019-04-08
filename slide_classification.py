0bc0270927b0a4a4b45f5c44924e00379f4457cf
Pooja Ahuja <pcomputo@Poojas-MacBook-Pro.local>
2019-04-07 17:34:39 -0700

    Added modules under single class

--- slide_classification.py
+++ slide_classification.py
@@ -15,34 +15,56 @@ IMAGE_HEIGHT = 128
 IMAGE_WIDTH = 128
 BATCH_SIZE = 32
 VALIDATION_SPLIT = 0.2
-EPOCHS = 5
+EPOCHS = 1
+STEPS_PER_EPOCH = 3
+VALIDATION_STEPS = 32
 
 
-#Labelled dataset creation
-train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, 
-	horizontal_flip = True, validation_split=VALIDATION_SPLIT)
-test_datagen = ImageDataGenerator(rescale = 1./255)
-training_set = train_datagen.flow_from_directory(DATASET_DIR, 
-	target_size = (IMAGE_HEIGHT, IMAGE_WIDTH), 
-	batch_size = BATCH_SIZE, class_mode = 'binary', subset='training')
-validation_generator = train_datagen.flow_from_directory(DATASET_DIR, 
-	target_size=(IMAGE_HEIGHT, IMAGE_WIDTH), batch_size=BATCH_SIZE,
-	class_mode='binary', subset='validation')
-
-#classifier architecture
-classifier = Sequential()
-classifier.add(Conv2D(32, (3, 3), input_shape = (IMAGE_HEIGHT, IMAGE_WIDTH, 3), 
-	activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
-classifier.add(Flatten())
-classifier.add(Dense(units = 1, activation = 'sigmoid'))
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+class SlideClassification():
 
 
-classifier.fit_generator(training_set, steps_per_epoch = 300, 
-	validation_data = validation_generator, validation_steps = 32, 
-	epochs = EPOCHS)
+	def generate(self):
+		#Labelled dataset creation
+		self.train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, 
+			horizontal_flip = True, validation_split=VALIDATION_SPLIT)
+		self.test_datagen = ImageDataGenerator(rescale = 1./255)
+		self.training_set = self.train_datagen.flow_from_directory(DATASET_DIR, 
+			target_size = (IMAGE_HEIGHT, IMAGE_WIDTH), 
+			batch_size = BATCH_SIZE, class_mode = 'binary', subset='training')
+		self.validation_generator = self.train_datagen.flow_from_directory(DATASET_DIR, 
+			target_size=(IMAGE_HEIGHT, IMAGE_WIDTH), batch_size=BATCH_SIZE,
+			class_mode='binary', subset='validation')
 
-classifier.save('model_5e.h5')
 
-model = load_model('model_5e.h5')
+	def train(self):
+		#classifier architecture
+		self.classifier = Sequential()
+		self.classifier.add(Conv2D(32, (3, 3), input_shape = (IMAGE_HEIGHT, IMAGE_WIDTH, 3), 
+			activation = 'relu'))
+		self.classifier.add(MaxPooling2D(pool_size = (2, 2)))
+		self.classifier.add(Flatten())
+		self.classifier.add(Dense(units = 1, activation = 'sigmoid'))
+		self.classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+
+		self.classifier.fit_generator(self.training_set, steps_per_epoch = STEPS_PER_EPOCH, 
+			validation_data = self.validation_generator, validation_steps = VALIDATION_STEPS, 
+			epochs = EPOCHS)
+
+		self.save(self.classifier, "model_e"+str(EPOCHS)+"_spe_"+str(STEPS_PER_EPOCH)+".h5")
+
+
+	def save(self, classifier, model):
+		#save model passed as param
+		classifier.save(model)
+
+
+	def load(self, model_name):
+		#load model passed as param
+		model = load_model(model_name)
+		return model
+
+
+classification = SlideClassification()
+classification.generate()
+classification.train()
